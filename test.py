06c0a9711eb9836ec7c408167f3c1b7a1e1e88d8
Gil <ghopecs@gmail.com>
2024-11-12 18:38:53 -0500

    update

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
 from dotenv import load_dotenv
+import logging
+import sys
 
 # Load OpenAI API Key
 load_dotenv()
@@ -8,8 +15,24 @@ api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
     raise ValueError("OpenAI API key is missing!")
 
-documents = SimpleDirectoryReader("books/").load_data()
-index = VectorStoreIndex.from_documents(documents)
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("books/").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+# documents = SimpleDirectoryReader("books/").load_data()
+# index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-response = query_engine.query("Some question about the data should go here")
+response = query_engine.query("Apollonian and Dionysian")
 print(response)
