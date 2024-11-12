06536fb84468e96ee38a1b5b089eb6eead19c90a
Gil <ghopecs@gmail.com>
2024-11-12 18:29:25 -0500

    update

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+import os
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from dotenv import load_dotenv
 
-# Load documents and build index
-documents = SimpleDirectoryReader(
-    "books/"
-).load_data()
+# Load OpenAI API Key
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OpenAI API key is missing!")
+
+documents = SimpleDirectoryReader("books/").load_data()
 index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("Some question about the data should go here")
+print(response)
